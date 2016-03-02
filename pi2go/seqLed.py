79e1f7bbf541889fa458453632c1122d85125441
root <root@raspberrypi.(none)>
2016-03-02 11:14:53 +0000

    change

---
+++ pi2go/seqLed.py
@@ -0,0 +1,56 @@
+import RPi.GPIO as GPIO
+import pi2go
+import time, os, sys
+
+GPIO.setmode(GPIO.BOARD)
+pi2go.init()
+
+ColourMode = ["w", "R", "G", "B", "Off"]
+Mode = 0
+pi2go.setAllLEDs(0, 0, 0)
+
+while True:
+    changeColourMode = pi2go.getSwitch()
+    if changeColourMode == True:
+        Mode = (Mode + 1)
+        if Mode > 4:
+            Mode = 0
+        print ("Current Mode is: " + ColourMode[Mode])
+        time.sleep(0.1)
+    else:
+        print ("Current mode is: " + ColourMode[Mode])
+        time.sleep(0.1)
+
+    if Mode == 0:
+        pi2go.setLED(0, 0, 0, 0)
+        pi2go.setLED(1, 0, 0, 0)
+        pi2go.setLED(2, 0, 0, 0)
+        pi2go.setLED(3, 0, 0, 0)
+        print('OFF')
+    elif Mode == 1:
+        pi2go.setLED(0, 4095, 4095, 4095)
+        pi2go.setLED(1, 4095, 4095, 4095)
+        pi2go.setLED(2, 4095, 4095, 4095)
+        pi2go.setLED(3, 4095, 4095, 4095)
+        print('WHITE')
+    elif Mode == 2:
+        pi2go.setLED(0, 4095, 0, 0)
+        pi2go.setLED(1, 4095, 0, 0)
+        pi2go.setLED(2, 4095, 0, 0)
+        pi2go.setLED(3, 4095, 0, 0)
+        print('RED')
+    elif Mode == 3:
+        pi2go.setLED(0, 0, 4095, 0)
+        pi2go.setLED(1, 0, 4095, 0)
+        pi2go.setLED(2, 0, 4095, 0)
+        pi2go.setLED(3, 0, 4095, 0)
+        print('GREEN')
+    elif Mode == 4:
+        pi2go.setLED(0, 0, 0, 4095)
+        pi2go.setLED(1, 0, 0, 4095)
+        pi2go.setLED(2, 0, 0, 4095)
+        pi2go.setLED(3, 0, 0, 4095)
+        print('BLUE')
+
+GPIO.cleanup()
+sys.exit()
